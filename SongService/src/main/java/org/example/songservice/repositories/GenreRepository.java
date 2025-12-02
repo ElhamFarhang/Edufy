e67f8416625975a49d38ad19c4cf3ddc456c41d0
ElhamFarhang <62466802+ElhamFarhang@users.noreply.github.com>
2025-12-02 13:17:18 +0100

    Delete SongService directory

--- SongService/src/main/java/org/example/songservice/repositories/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.songservice.repositories;
-
-import org.example.songservice.entities.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-}
