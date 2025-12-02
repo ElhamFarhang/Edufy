21e7a6df32f7f476aa957a06d976fa1d879c57a0
ElhamFarhang <62466802+ElhamFarhang@users.noreply.github.com>
2025-12-02 13:07:26 +0100

    Delete AlbumService directory

--- AlbumService/src/main/java/org/example/albumservice/services/albums/AlbumService.java
+++
@@ -1,9 +0,0 @@
-package org.example.albumservice.services.albums;
-
-import java.util.List;
-
-import org.example.albumservice.dtos.albums.*;
-
-public interface AlbumService {
-    List<?> getAllAlbums(String details);
-}
